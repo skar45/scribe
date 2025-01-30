105cd1f97f7d2322b35d7a5b8ad7cf02f807034f
Sajal <karkee455@gmail.com>
2025-01-29 22:21:11 -0500

    Parsing epub

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,19 +1,44 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use std::{error::Error, fs::File, path::Path};
+
+use zip::ZipArchive;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
 #[tauri::command]
-fn greet2(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn read_files(files: Vec<&str>) -> Vec<String> {
+    let mut contents: Vec<String> = Vec::new();
+    for file in files {
+        let path = Path::new(file);
+        if let Some(ext) = path.extension() {
+            if ext == "epub"{
+                contents.push(process_epub(path).unwrap());
+            }
+        }
+    }
+    contents
+}
+
+fn process_epub(path: &Path) -> Result<String, Box<dyn Error>> {
+    let file = File::open(path)?;
+    let mut zip = ZipArchive::new(file)?;
+    let mut buf = String::new();
+    for i in 0..zip.len() {
+        let file = zip.by_index(i)?;
+        buf.push_str(file.name());
+        buf.push_str(", ");
+    }
+    Ok(buf)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, read_files])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
